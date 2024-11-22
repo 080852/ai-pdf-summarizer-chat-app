bbcacc80eea96e01c1eb2f0ac3f892f3ee3420e2
080852 <138621328+080852@users.noreply.github.com>
2024-11-23 01:15:54 +0530

    make changes done to make a table in convexdb

--- app/dashboard/_components/UploadPdfDialog.js
+++ app/dashboard/_components/UploadPdfDialog.js
@@ -1,20 +1,22 @@
 "use client"
 import { Button } from '@/components/ui/button'
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from '@/components/ui/input'
 import { api } from '@/convex/_generated/api'
+import { useUser } from '@clerk/nextjs'
 import { useMutation } from 'convex/react'
 import { Loader2Icon } from 'lucide-react'
 import { useState } from 'react'
+import uuid4 from 'uuid4'
 
 
   
@@ -22,8 +24,13 @@ import { useState } from 'react'
 function UploadPdfDialog({children}) {
 
     const generateUploadUrl=useMutation(api.fileStorage.generateUploadUrl);
+    const addFileEntry=useMutation(api.fileStorage.AddFileEntryToDb);
+    const {user}=useUser();
     const [file,setFile]=useState();
     const [loading, setLoading]=useState(false);
+    const [fileName,setFileName]=useState();
+
+
     const OnFileSelect=(event)=>{
         setFile(event.target.files[0]);
 
@@ -42,7 +49,17 @@ function UploadPdfDialog({children}) {
       });
       const { storageId } = await result.json();
       console.log('StorageId',storageId);
+      const fileId=uuid4();
+      // Step 3: Save the newly allocated storage id to the database
 
+        const resp=await addFileEntry({
+          fileId:fileId,
+          storageId:storageId,
+          fileName:fileName??'Untitle File',
+          createdBy:user?.primaryEmailAddress?.emailAddress
+
+        })
+        console.log(resp);
       setLoading(false);
 
     }
@@ -64,7 +81,7 @@ function UploadPdfDialog({children}) {
             </div>
             <div className='mt-2'>
                 <label>File Name *</label>
-                <Input placeholder="File Name"/>
+                <Input placeholder="File Name" onChange={(e)=>setFileName(e.target.value)}/>
             </div>
            
 
--- convex/fileStorage.js
+++ convex/fileStorage.js
@@ -1,5 +1,25 @@
+import { v } from "convex/values";
 import { mutation } from "./_generated/server";
 
 export const generateUploadUrl = mutation(async (ctx) => {
   return await ctx.storage.generateUploadUrl();
 });
+
+export const AddFileEntryToDb=mutation({
+  args:{
+    fileId:v.string(),
+    storageId:v.string(),
+    fileName:v.string(),
+    cretedBy:v.string()
+  },
+  handler:async(ctx,agrs)=>{
+       const result=await ctx.db.insert('pdfFiles',{
+        fileId:args.fileId,
+        fileName:args.fileName,
+        storageId:args.storageId,
+        createdBy:args.createdBy
+
+       })
+       return 'Inserted'
+  }
+})
